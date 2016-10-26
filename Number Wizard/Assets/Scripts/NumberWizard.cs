6ca1fa718ca2c68e3259a1a71df80a4fc577e382
Mike Ligthart <mike.ligthart@gmail.com>
2016-10-26 20:28:24 +0200

    adds start of Number Wizard project (first project of unity course)

---
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int max = 1000;
+		int min = 1;
+		
+		print ("Welcome to the number wizard");
+		print ("Think of a number between " + min + "and " + max + " and don't tell me");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
