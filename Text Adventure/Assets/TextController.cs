6969d9042b25b9c608e102cf5a998380959815ab
Mike Ligthart <mike.ligthart@gmail.com>
2016-10-28 07:31:49 +0200

    adds base code for Text Adventure game from the course

---
+++ Text Adventure/Assets/TextController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+	
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello world";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)){
+			text.text = "You are in a prison cell, and you want to escape. " + 
+						"There are some dirty sheets on the bed, a mirror on the wall, " +
+						"and the door is locked from the outside.\n\n" +
+						"Press S to view the sheets, M to view the mirror and L to view the lock";
+		}
+		
+		if (Input.GetKeyUp (KeyCode.Space)){
+			text.text = "Hello world again";
+		}
+	}
+}
